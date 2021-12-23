f78dd7f40fc123c68a8362a25b0560e64c0e9e07
boboxu <boboxu5858@gmail.com>
2021-12-22 22:53:11 -0500

    hw1_algorithms 3 questions

--- hw1_algorithms.py
+++ hw1_algorithms.py
@@ -3,3 +3,29 @@ def hw1sum(n):
     for i in range(n):
         summary += i
     return summary
+
+
+# 2. Find max number from 3 values, entered manually from a keyboard.
+# Example: 124, 21, 32. Result = 124.
+arr = []
+for i in range(3):
+    n = int(input("Please enter a number:"))
+    arr.append(n)
+    if i == 0 or n > maxN:
+        maxN = n
+print(f"Largest element present in {arr} is: " + str(maxN))
+
+
+# 3.Count odd and even numbers. Count odd and even digits of the whole number.
+# Example: entered number is 34560, then 3 digits will be even (4, 6, and 0) and 2 odd digits (3 and 5).
+odd = 0
+even = 0
+num = int(input("Please enter a number:"))
+x = [int(a) for a in str(num)]
+print(x)
+for i in x:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print(f"odd numbers {odd}, even numbers {even}")
